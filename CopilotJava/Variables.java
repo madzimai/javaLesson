e2a24aa8af564f639ac206bfbfc2ad1c3b4a81b6
Netsai <hellonetsai@gmail.com>
2025-01-24 09:54:33 +0200

    Variable declaration

---
+++ CopilotJava/Variables.java
@@ -0,0 +1,12 @@
+package CopilotJava;
+
+public class Variables {
+    String name, value;
+
+    //int number; num; // the variables are in same line but they are seperated by semicolon so the second on doesn't have the data type like on line 8
+
+    int number2; int number3;
+
+    //double number4, double fig; // two data type must not be in same line and seperated by a comma so they should like line 4
+
+}
