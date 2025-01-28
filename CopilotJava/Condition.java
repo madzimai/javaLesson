9bf594364216934212920e3f823dbaadfef67baa
Netsai <hellonetsai@gmail.com>
2025-01-28 02:41:43 +0200

    one identation statement is valid for if-then conditon wn no {}

---
+++ CopilotJava/Condition.java
@@ -0,0 +1,10 @@
+package CopilotJava;
+
+public class Condition {
+    public static void main(String[] args) {
+        int num = 11;
+        if (num > 10)
+            System.out.println(num + " is greater than 10");
+
+    }
+}
