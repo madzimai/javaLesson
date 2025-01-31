df09f7073671116668de499903b41c48d31ec555
Netsai <hellonetsai@gmail.com>
2025-01-31 10:34:34 +0200

    Code practise

---
+++ OfflinePrac/Abst.java
@@ -0,0 +1,9 @@
+package OfflinePrac;
+
+abstract class Abst {
+    public void take(){
+        System.out.print("take");
+    }
+
+    abstract void eat ();
+}
---
+++ OfflinePrac/Self.java
@@ -0,0 +1,24 @@
+package OfflinePrac;
+import java.time.*;
+
+public class Self implements Off{
+    public void off() {
+    }
+    public void eat(){
+        System.out.println("date");
+    }
+    public static void main(String[] args) {
+        Off off = new Self();
+        off.off();
+
+        LocalDate date = LocalDate.parse("2020-01-02");
+        System.out.println(date);
+
+        LocalDateTime dT = LocalDateTime.parse("2020-01-02T21:20");
+        System.out.println(dT);
+
+        LocalTime time = LocalTime.parse("12:30");
+        System.out.println(time);
+
+    }
+}
