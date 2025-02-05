d39a127db1931a6e5c89506b0695c982896239fb
Netsai <hellonetsai@gmail.com>
2025-02-05 02:06:28 +0200

    Set length of a stringBuilder with setLength()

--- Write/Test.java
+++ Write/Test.java
@@ -10,12 +10,19 @@ public class Test {
     public void showTwo(int myValue){
         this.myValue = myValue;
     }
+
     public static void main(String[] args) {
+
         Test ct = new Test();
         ct.showOne(200);
         System.out.println(ct.myValue);
         ct.showTwo(100);
         System.out.println(ct.myValue);
+
+        StringBuilder sb = new StringBuilder("12345678");
+       sb.setLength(5);
+        sb.setLength(10);
+        System.out.println(sb);
     }
 
 }
