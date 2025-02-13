5f756edf8008554079d70eac2a3e96c9c61b64bd
Netsai <hellonetsai@gmail.com>
2025-02-13 11:22:39 +0200

    Use equals() and == to compare string objects - find reasons for each outcome result

---
+++ JavaApi/Methods.java
@@ -0,0 +1,18 @@
+package JavaApi;
+
+public class Methods {
+    String name;
+    public static void main(String[] args) {
+        String x = "Hello World";
+        String z = " Hello World".trim();
+        System.out.println(x.equals(z));//equals to check the values inside the String
+        System.out.println(x==z); //this check the object reference. 🙌
+
+        Methods t1 = new Methods();
+        Methods t2 = new Methods();
+        Methods t3 = t1;
+        System.out.println(t1 == t1);// true because we are comparing same ref
+        System.out.println(t1.equals(t2));// the class does not implement equals()
+        System.out.println(t1 == t2);// false because the ref are different
+    }
+}
