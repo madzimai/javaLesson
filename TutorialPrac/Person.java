d9592f3db309e4e12c39a6a7889acbc4d35ab34d
Netsai <hellonetsai@gmail.com>
2025-04-12 15:27:36 +0200

    Add test to class person

--- TutorialPrac/Person.java
+++ TutorialPrac/Person.java
@@ -1,4 +1,12 @@
 package TutorialPrac;
 
 public class Person {
+    private String name;
+    private int age;
+     public String helloPerson(String marcus){
+         return "Hello"+ marcus;
+     }
+     public String helloPerson2(){
+         return "Hello World";
+     }
 }
--- TutorialPrac/PersonTest.java
+++ TutorialPrac/PersonTest.java
@@ -1,4 +1,19 @@
 package TutorialPrac;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PersonTest {
+
+    @Test
+    public void testPerson() {
+        Person person = new Person();
+        assertEquals("Hello world", person.helloPerson2());
+    }
+
+    @Test
+    public void shouldReturnMarcus(){
+        Person person = new Person();
+        assertEquals("Hello world", person.helloPerson("Marcus"));
+    }
 }
