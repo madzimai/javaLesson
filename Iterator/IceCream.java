39d1506db58ee42469194f29e3957e4922c33784
Netsai <hellonetsai@gmail.com>
2025-04-14 23:32:11 +0200

    Switch statement eliminate break iteration

---
+++ Iterator/IceCream.java
@@ -0,0 +1,16 @@
+package Iterator;
+
+public class IceCream {
+    public static void main(String[] args) {
+
+        int flavours = 30;
+        int eaten = 0;
+        switch (flavours) {
+            case 30: eaten++;
+            case 40: eaten+=2;
+            default: eaten--;
+
+        }
+        System.out.println(eaten);
+    }
+}
